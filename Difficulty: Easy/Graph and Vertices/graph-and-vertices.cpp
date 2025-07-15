1648f957804fe06e2d4ea5f65b9c9d48ad3aad1e
NISHANT PANDEY <nishant45p@gmail.com>
2025-07-15 10:58:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Graph and Vertices/graph-and-vertices.cpp
@@ -0,0 +1,12 @@
+// User function Template for C++
+class Solution {
+  public:
+    long long count(int n) {
+        // your code here
+        if(n <= 2 ) return n;
+        int edge = n*(n-1)/2;
+        
+        return pow(2,edge);
+        
+    }
+};
