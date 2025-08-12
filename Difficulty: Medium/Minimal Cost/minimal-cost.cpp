43687026f93b43174615f90b593681cf7f196265
NISHANT PANDEY <nishant45p@gmail.com>
2025-08-12 12:17:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimal Cost/minimal-cost.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int minimizeCost(int k, vector<int>& arr) {
+        // Code here
+        int n = arr.size();
+        vector<int> dp(n,0);
+        dp[0] = 0;
+        for(int i = 1 ;i<n ; i++)
+        {
+            int minStep = INT_MAX;
+            for(int j = 1 ; j <= k ; j++)
+            { 
+                if(i-j >= 0) 
+                { 
+                    int jump = dp[i-j] + abs(arr[i] - arr[i-j]);
+                    minStep = min(minStep , jump);
+                }
+                dp[i] = minStep;
+            }
+        }
+        return dp[n-1];
+        
+    }
+};
