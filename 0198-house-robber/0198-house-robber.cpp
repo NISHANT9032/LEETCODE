7820248f24375789cde1e48bc834e2415b492829
NISHANT PANDEY <nishant45p@gmail.com>
2025-08-13 11:10:06 +0530

    Time: 0 ms (100%), Space: 10.2 MB (80.37%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int prev=nums[0] , prev2 = 0;
+        for(int i = 1 ; i<n ; i++)
+        { 
+            int take = nums[i];
+            if(i>1) take += prev2;
+            int non_take = 0 + prev ;
+            int curr = max(take , non_take);
+            prev2 = prev ; 
+            prev = curr;
+        }
+        return prev;
+    }
+};
