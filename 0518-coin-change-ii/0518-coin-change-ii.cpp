91143d79d9ef875844f6f7b04e5f0f7282037824
NISHANT PANDEY <nishant45p@gmail.com>
2025-08-22 12:58:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+
+        for (int coin : coins) {
+            for (int t = coin; t <= amount; t++) {
+                dp[t] += dp[t - coin];
+            }
+        }
+        return dp[amount];
+    }
+};
