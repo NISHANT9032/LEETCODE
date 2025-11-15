ece45a665ea68342796d07553455e64bcd3bca61
NISHANT PANDEY <nishant45p@gmail.com>
2025-11-15 22:18:11 +0530

    Time: 137 ms (5.57%), Space: 126.2 MB (100%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        //int n=nums.size();
+        vector <int>ans(nums.size());
+        int posIndex=0;
+        int negIndex=1;
+        for(int i=0;i<nums.size();i++)
+        {
+            if (nums[i]<0)
+            {
+                ans[negIndex]=nums[i];
+                negIndex +=2;
+            }
+            else{
+                ans[posIndex]=nums[i];
+                posIndex +=2;
+            }
+        }
+        return ans;
+    }
+};
