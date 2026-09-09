ba796a475219a946de9b6137db219e16e986ec41
NISHANT PANDEY <nishant45p@gmail.com>
2026-09-10 00:37:16 +0530

    Time: 72 ms (53.38%), Space: 111.4 MB (28.29%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int>st;
+        for(int i : nums)
+        { 
+            if(st.count(i) > 0) return true;
+            st.insert(i);
+            
+        }
+        return false ;
+
+    }
+};
